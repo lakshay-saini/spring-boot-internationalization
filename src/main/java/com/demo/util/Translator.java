00108d9a0574f6141b6f6e9d4f30826d0ee8e7d7
Lakshay Saini <39418344+lakshay-saini@users.noreply.github.com>
2022-08-03 19:40:45 +0530

    change messageSource type

--- src/main/java/com/demo/util/Translator.java
+++ src/main/java/com/demo/util/Translator.java
@@ -2,7 +2,7 @@ package com.demo.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
@@ -10,10 +10,10 @@ import java.util.Locale;
 @Component
 public class Translator {
 
-	private static ResourceBundleMessageSource messageSource;
+	private static ReloadableResourceBundleMessageSource messageSource;
 
 	@Autowired
-	Translator(ResourceBundleMessageSource messageSource) {
+	Translator(ReloadableResourceBundleMessageSource messageSource) {
 		Translator.messageSource = messageSource;
 	}
 
